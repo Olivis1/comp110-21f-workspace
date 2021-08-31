a1422c9c0c59f2c40616ed896944a4c30b9b4208
Olivia Chatfield <olivis@ad.unc.edu>
2021-08-31 14:18:25 -0400

    Progess on lesson 1

--- exercises/ex01/numeric_operators.py
+++ exercises/ex01/numeric_operators.py
@@ -4,9 +4,18 @@ __author__: str = "730530311"
 
 left: str = str(input("Left-hand side:"))
 right: str = str(input("Right-hand side:"))
-stars: str = "** "
+left_int: int = int(left)
+right_int: int = int(right) 
+exponent: int = int(left_int ** right_int)
+exponent_str: str = str(exponent)
+slash: float = float(left_int / right_int)
+slash_str: str = str(slash)
+two_slash: int = int(left_int // right_int)
+two_slash_str: str = str(two_slash)
+percent: int = int(left_int % right_int)
+percent_str: str = str(percent)
 
-print((left + stars + right) + "is " + (int(left ** right)))
-print(right / left)
-print(right // left)
-print(right % left)
+print(left + " ** " + right + " is " + exponent_str)
+print(left + " / " + right + " is " + slash_str)
+print(left + " // " + right + " is " + two_slash_str)
+print(left + " % " + right + " is " + percent_str)
--- exercises/ex01/relational_operators.py
+++ exercises/ex01/relational_operators.py
@@ -2,8 +2,20 @@
 
 __author__: str = "730530311"
 
-left_side: str = "Left-hand side: " (input)
-right_side: str = "Right-hand side: 5" (input)
+left: str = str(input("Left-hand side:"))
+right: str = str(input("Right-hand side:"))
+left_bool: bool = bool(left)
+right_bool: bool = bool(right)
+arrow: bool = bool(left_bool < right_bool)
+arrow_str: str = str(arrow)
+at_least: bool = bool(left_bool >= right_bool)
+at_least_str: str = str(at_least)
+equal: bool = bool(left_bool == right_bool)
+equal_str: str = str(equal)
+not_equal: bool = bool(left_bool != right_bool)
+not_equal_str: str = str(not_equal)
 
-print
-
+print(left + " < " + right + " is " + arrow_str)
+print(left + " >= " + right + " is " + at_least_str)
+print(left + " == " + right + " is " + equal_str)
+print(left + " != " + right + " is " + not_equal_str)
