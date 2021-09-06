d35a63d9122902fe2c6a5c72bdab054fccf2450c
Olivia Chatfield <olivis@ad.unc.edu>
2021-09-06 16:34:43 -0400

    count letter changes

--- exercises/ex02/count_letters.py
+++ exercises/ex02/count_letters.py
@@ -1,6 +1,17 @@
 """Counting letters in a string."""
 
-__author__ = "ENTER YOUR 9-DIGIT PID HERE"
+__author__ = "730530311"
 
-
+# from typing import Sequence
 # Begin your solution here...
+# python -m exercises.ex02.count_letters
+letter: str = str(input("What letter do you want to search for?: "))
+word: str = str(input("Enter a word: "))
+i: int = 0
+count: int = int(word * len(letter))
+
+
+while i < len(word):
+    print(word[i])
+    i = i + 1
+  
