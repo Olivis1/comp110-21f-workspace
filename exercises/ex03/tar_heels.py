fbc548e4c7f10268b7146631c975019c32412221
Olivia Chatfield <olivis@ad.unc.edu>
2021-09-13 19:54:59 -0400

    message on tar

--- exercises/ex03/tar_heels.py
+++ exercises/ex03/tar_heels.py
@@ -1,6 +1,26 @@
 """An exercise in remainders and boolean logic."""
 
-__author__ = "ENTER YOUR 9-DIGIT PID HERE"
+__author__ = "730530311"
 
 
-# Begin your solution here...
+a: int = int(input("Enter an int: "))
+
+b: int = int(a % 2)
+c: int = int(a % 7)
+d: int = int(c + b)
+i: int = 1
+# python -m exercises.ex03.tar_heels
+
+while i >= 1:
+    if d == 0:
+        print("TAR HEELS")
+    else:
+        if b == 0:
+            print("TAR")
+        else:
+            if c == 0:
+                print("HEELS")
+            else:
+                if d >= 2:
+                    print("CAROLINA")
+    i: int = i - i
